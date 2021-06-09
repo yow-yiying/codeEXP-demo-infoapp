fce94ee55afd8f4d86f6e97b298ccc23f6367806
Yow Yiying <yowyiying@gmail.com>
2021-06-09 19:51:05 +0800

    Added contacts screen

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "./Screens/Home";
+import ContactsScreen from "./Screens/Contact";
+import EventsScreen from "./Screens/Event";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style = {{color: "red", fontWeight: "bold"}}>Hellow World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Events" component={EventsScreen} />
+        <Tab.Screen name="Contact" component={ContactsScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Screens/Contact.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {Text, View} from "react-native";
+
+export default function ContactsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' , backgroundColor: "lightgreen"}}>
+        <Text>Contacts!</Text>
+      </View>
+    );
+}
